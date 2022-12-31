ca98f7c9a7e953e9192fecbe9189fa16481ad3a2
Willian Ricardo <willianricardoleal@gmail.com>
2022-12-31 15:43:49 -0300

    Criação da listagem dos contatos e do novo contato

---
+++ Controllers/ContatosController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Context;
+
+namespace ProjetoMVC.Controllers
+{
+    public class ContatosController : Controller
+    {
+        private readonly AgendaContext _context;
+
+        public ContatosController(AgendaContext context)
+        {
+            _context = context;
+        }
+         public IActionResult Index()
+        {
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+    }
+}
